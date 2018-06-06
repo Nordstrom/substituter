255ad25b1b2bf158318deff7307fe92db17b0277
Andy Day <andy.day@nordstrom.com>
2018-06-06 12:29:12 -0700

    update dependencies and add standardjs support

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-'use strict';
-
-var _ = require('lodash');
+const _ = require('lodash')
 
 module.exports = function (tmpl, data) {
-    function substitute(p) {
-        var r = p.replace(/\$\{([\w\.\-]+)}/g, function (match, term) {
-            return _.get(data, term) || match;
-        });
+  function substitute (p) {
+    const r = p.replace(/\$\{([\w.-]+)}/g, function (match, term) {
+      return _.get(data, term) || match
+    })
 
-        if (_.startsWith(r, '/') && _.endsWith(r, '/')) {
-            return new RegExp(_.trim(r, '/'));
-        }
-
-        return r;
+    if (_.startsWith(r, '/') && _.endsWith(r, '/')) {
+      return new RegExp(_.trim(r, '/'))
     }
 
-    function transform(obj) {
-        if (obj === undefined || obj === null) return obj;
-        if (_.isBuffer(obj)) return substitute(obj.toString());
-        if (_.isString(obj)) return substitute(obj);
-        if (_.isArray(obj)) return _.map(obj, transform);
-        if (_.isPlainObject(obj)) return _.mapValues(obj, transform);
-        return obj;
-    }
+    return r
+  }
 
-    return transform(tmpl);
-};
+  function transform (obj) {
+    if (obj === undefined || obj === null) return obj
+    if (_.isBuffer(obj)) return substitute(obj.toString())
+    if (_.isString(obj)) return substitute(obj)
+    if (_.isArray(obj)) return _.map(obj, transform)
+    if (_.isPlainObject(obj)) return _.mapValues(obj, transform)
+    return obj
+  }
+
+  return transform(tmpl)
+}
--- test.js
+++ test.js
@@ -1,67 +1,65 @@
-'use strict';
+const should = require('should')
+const fs = require('fs')
+const sub = require('./')
 
-var should = require('should'),
-    fs = require('fs'),
-    sub = require('./');
-
+/* eslint-disable no-template-curly-in-string */
 describe('substituter', function () {
-    it('should sub at root', function () {
-        sub('hello ${region}!', { region: 'world' }).should.equal('hello world!');
-    });
+  it('should sub at root', function () {
+    sub('hello ${region}!', { region: 'world' }).should.equal('hello world!')
+  })
 
-    it('should sub at level 1', function () {
-        sub('hello ${globe.region}!', { globe: { region: 'world' } }).should.equal('hello world!');
-    });
+  it('should sub at level 1', function () {
+    sub('hello ${globe.region}!', { globe: { region: 'world' } }).should.equal('hello world!')
+  })
 
-    it('should pass thru when not found', function () {
-        sub('hello ${globe.unknown}!', {}).should.equal('hello ${globe.unknown}!');
-    });
+  it('should pass thru when not found', function () {
+    sub('hello ${globe.unknown}!', {}).should.equal('hello ${globe.unknown}!')
+  })
 
-    it('should pass thru on undefined', function () {
-        sub('hello ${globe.unknown}!').should.equal('hello ${globe.unknown}!');
-    });
+  it('should pass thru on undefined', function () {
+    sub('hello ${globe.unknown}!').should.equal('hello ${globe.unknown}!')
+  })
 
-    it('should sub into xml file', function () {
-        sub(fs.readFileSync('./test.xml'), { globe: { region: 'world' } }).should.equal('<test><testel1>world</testel1></test>');
-    });
+  it('should sub into xml file', function () {
+    sub(fs.readFileSync('./test.xml'), { globe: { region: 'world' } }).should.equal('<test><testel1>world</testel1></test>')
+  })
 
-    it('should handle undefined tmpl gracefully', function () {
-        should.equal(sub(undefined, undefined), undefined);
-    });
+  it('should handle undefined tmpl gracefully', function () {
+    should.equal(sub(undefined, undefined), undefined)
+  })
 
-    it('should handle null tmpl gracefully', function () {
-        should.equal(sub(null, undefined), null);
-    });
+  it('should handle null tmpl gracefully', function () {
+    should.equal(sub(null, undefined), null)
+  })
 
-    it('should handle undefined replacements gracefully', function () {
-        should.equal(sub('blah', undefined), 'blah');
-    });
+  it('should handle undefined replacements gracefully', function () {
+    should.equal(sub('blah', undefined), 'blah')
+  })
 
-    it('should sub into object at level 1', function () {
-        sub({ name: '${param}' }, { param: 'param-value' }).should.eql({ name: 'param-value' });
-    });
+  it('should sub into object at level 1', function () {
+    sub({ name: '${param}' }, { param: 'param-value' }).should.eql({ name: 'param-value' })
+  })
 
-    it('should sub into object at level 3', function () {
-        sub({ obj1: { obj2: { name: '${params1.params2.param}' } } }, { params1: { params2: { param: 'param-value' } } })
-            .should.eql({ obj1: { obj2: { name: 'param-value' } } });
-    });
+  it('should sub into object at level 3', function () {
+    sub({ obj1: { obj2: { name: '${params1.params2.param}' } } }, { params1: { params2: { param: 'param-value' } } })
+      .should.eql({ obj1: { obj2: { name: 'param-value' } } })
+  })
 
-    it('should sub regex into object', function () {
-        sub({ name: '${param}' }, { param: /xyz/ }).should.eql({ name: /xyz/ });
-    });
+  it('should sub regex into object', function () {
+    sub({ name: '${param}' }, { param: /xyz/ }).should.eql({ name: /xyz/ })
+  })
 
-    it('should sub for array of objects', function () {
-        sub([{ name: '${param1}' }, { name: '${param2}' }], {
-            param1: 'p1',
-            param2: 'p2'
-        }).should.eql([{ name: 'p1' }, { name: 'p2' }]);
-    });
+  it('should sub for array of objects', function () {
+    sub([{ name: '${param1}' }, { name: '${param2}' }], {
+      param1: 'p1',
+      param2: 'p2'
+    }).should.eql([{ name: 'p1' }, { name: 'p2' }])
+  })
 
-    it('should sub for object with array of objects', function () {
-        sub({ obj: [{ name: '${param1}' }, { name: '${param2}' }] }, {
-            param1: 'p1',
-            param2: 'p2'
-        }).should.eql({ obj: [{ name: 'p1' }, { name: 'p2' }] });
-    });
-
-});
+  it('should sub for object with array of objects', function () {
+    sub({ obj: [{ name: '${param1}' }, { name: '${param2}' }] }, {
+      param1: 'p1',
+      param2: 'p2'
+    }).should.eql({ obj: [{ name: 'p1' }, { name: 'p2' }] })
+  })
+})
